c1831ed0f65e46d2fe18fa0ad1e9a582d4e014cf
prisca <priscaakaniro@gmail.com>
2026-02-27 16:26:09 +0000

    Hurray, game is done on basic level. Works as expected with minimal code, just functions. Error handling next.

--- randomNumberGame.py
+++ randomNumberGame.py
@@ -1,3 +1,47 @@
 #Making a number guessing game just to get back into coding
+import random #to randomly generate numbers
+
+Username = input("Enter your name: ")#stores users name
+number = 0 #stores their guess
+actualNumber = 0 #python generated number
+attempts = 3 #number of attempts user has
+
+#Introductory message
 def intro():
-    print("Welcome to the game")
+    print(f"Welcome to the guessing game {Username}\nInstructions:\nEnter a number between 1 and 5.\n"
+          "If the number you entered matches the generated number, you win. Else you lose.\n3 attempts\n")
+#generating num
+def printNum():
+    global actualNumber
+
+    actualNumber = random.randint(1, 5)
+    return actualNumber
+
+#getting user num
+def getNum():
+    global number
+
+    number = int(input("Enter a number between 1 and 5: "))
+    return number
+
+#comparing user entry to see if they guessed correctly
+def numberEntry():
+    global attempts
+
+    while attempts > 0:
+        if number == actualNumber:
+            print(f"\nCongratulations {Username}, you won!")
+            exit()
+        elif number != actualNumber:
+            attempts -= 1
+            print(f"\nSorry, {Username}, you lost!\n"
+                  f"Number was {actualNumber}.\nYou have {attempts} attempts left\n")
+            getNum()
+            printNum()
+        else:
+            print(f"\nSorry, {Username}, you lost. Better luck next time.")
+
+#basically main: starting the game
+intro()
+getNum()
+numberEntry()

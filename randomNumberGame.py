ff0bc7a1cee01e0e53fe4fb90a02672da6e1bcaa
prisca <priscaakaniro@gmail.com>
2026-03-04 21:06:24 +0000

    Hurray, game is done on basic level. Works as expected with minimal code, just functions. Error handling next.

--- randomNumberGame.py
+++ randomNumberGame.py
@@ -2,46 +2,46 @@
 import random #to randomly generate numbers
 
 Username = input("Enter your name: ")#stores users name
-number = 0 #stores their guess
-actualNumber = 0 #python generated number
-attempts = 3 #number of attempts user has
 
 #Introductory message
 def intro():
     print(f"Welcome to the guessing game {Username}\nInstructions:\nEnter a number between 1 and 5.\n"
           "If the number you entered matches the generated number, you win. Else you lose.\n3 attempts\n")
-#generating num
-def printNum():
-    global actualNumber
-
-    actualNumber = random.randint(1, 5)
-    return actualNumber
 
 #getting user num
 def getNum():
     global number
 
-    number = int(input("Enter a number between 1 and 5: "))
-    return number
+    while True:
+        try:
+            number = int(input("Enter a number between 1 and 5: "))
+            if 1 <= number <= 5:
+                return number
+            else:
+                print("Please stay within the range (1-5).")
+        except ValueError:
+            print("Invalid input! Please enter a whole number.")
 
 #comparing user entry to see if they guessed correctly
-def numberEntry():
-    global attempts
+def play():
+    intro()
+    attempts = 3 #number of attempts user has
 
     while attempts > 0:
+        number = getNum()
+        actualNumber = random.randint(1, 5)
+
         if number == actualNumber:
             print(f"\nCongratulations {Username}, you won!")
-            exit()
-        elif number != actualNumber:
-            attempts -= 1
-            print(f"\nSorry, {Username}, you lost!\n"
-                  f"Number was {actualNumber}.\nYou have {attempts} attempts left\n")
-            getNum()
-            printNum()
+            return
         else:
-            print(f"\nSorry, {Username}, you lost. Better luck next time.")
+            attempts -= 1
+            if attempts > 0:
+                print(f"Wrong! The number was {actualNumber}.")
+                print(f"You have {attempts} attempts left.\n")
+            else:
+                print(f"Wrong! The number was {actualNumber}.")
+                print(f"\nGame Over, {Username}. You've run out of attempts.")
 
 #basically main: starting the game
-intro()
-getNum()
-numberEntry()
+play()

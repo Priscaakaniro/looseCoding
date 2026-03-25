d1fc937fcdaba7d42c22eb4f11542ce3cd6c7a55
prisca <priscaakaniro@gmail.com>
2026-03-25 23:05:00 +0000

    took a break, but back at it

--- game/game.cpp
+++ game/game.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-void intro(string name);
-char PlayerAction(char action);
+void intro() {
+	string name;
+	cout << "What is your name: ";
+	getline(cin, name);
+	cout << "Welcome to the game "<< name<< endl 
+		<< "You have 3 options to defeat the villian stalking you and to keep fighting: Attack, Heal or Run"<<endl
+		<<"You choose to attack, you loose health points."<< endl<< "You choose to run, game ends."<< endl
+		<<"You choose to heal, you restore some of your health points to fight again."<<endl<<endl
+		<<"Things to note: "<<endl<<"10 total Health points."<<endl<<"1 Heal opportunity."<<endl<<"GOODLUCK!"<<endl;
+}
 
+/*i want to do a class that would have the players and the computers actions like choices, health, heal*/
+class player {
+public:
+	char choice;
+	int health;
+	int heal;
+};
+
+class villain {
+public:
+	char choice;
+	int health;
+	int heal;
+};
 
 int main() {
-	cout << "Hi!";
+	intro();
 	return 0;
 }
